f550ac73e3a9589c7da99d81424181573f90ec01
kaioz11 <hoangmai.tq2000@gmail.com>
2021-01-03 13:51:18 +0700

    test

---
+++ main.py
@@ -0,0 +1,70 @@
+import pygame 
+from pygame.locals import *
+import sys
+
+WINDOWWIDTH = 400 # Chiều dài cửa sổ
+WINDOWHEIGHT = 300 # Chiều cao cửa sổ
+
+WHITE = (255, 255, 255)
+RED   = (255,   0,   0)
+GREEN = (  0, 255,   0)
+
+pygame.init()
+
+### Xác định FPS ###
+FPS = 60
+fpsClock = pygame.time.Clock()
+
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption('Ninja piu piu')
+
+class Ninja():
+    def __init__(self):
+        self.x = 0 # Vị trí của xe
+
+        ## Tạo surface và thêm hình chiếc xe vào ##
+        self.surface = pygame.image.load('ninja2.png')
+    
+    def draw(self): # Hàm dùng để vẽ xe
+        DISPLAYSURF.blit(self.surface, (self.x, 100))
+
+    def update(self, moveLeft, moveRight): # Hàm dùng để thay đổi vị trí xe
+        if moveLeft == True:
+            self.x -= 2
+        if moveRight == True:
+            self.x += 2
+        
+        if self.x + 100 > WINDOWWIDTH:
+            self.x = WINDOWWIDTH - 100
+        if self.x < 0:
+            self.x = 0
+
+Ninja = Ninja()
+moveLeft = False
+moveRight = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                moveLeft = True
+            if event.key == K_RIGHT:
+                moveRight = True
+
+        if event.type == KEYUP:
+            if event.key == K_LEFT:
+                moveLeft = False
+            if event.key == K_RIGHT:
+                moveRight = False        
+    
+    DISPLAYSURF.fill(WHITE)
+    
+    Ninja.draw()
+    Ninja.update(moveLeft, moveRight)
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
