e5dd14af1780be830b0c0a74d69ae72091c3b5bb
kaioz11 <hoangmai.tq2000@gmail.com>
2021-01-07 22:23:39 +0700

    add prop

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ pygame.display.set_caption('Ninja piu piu')
 
 # self.surface = pygame.image.load('./data/img1/NINJA03GIF-0000.png')
 paht_img = './data/img1/'
+paht_img1 = './data/img/'
 
 backgroud = pygame.image.load(paht_img + 'bg.png')
 char = pygame.image.load(paht_img + 'NINJA03GIF-0000.png')
@@ -85,57 +86,77 @@ class Ninja():
             if self.left:
                 DISPLAYSURF.blit(walkLeft[self.walkCount // 3], (self.x,self.y))
                 self.walkCount += 1
-                # print(self.walkCount)
-
             elif self.right:
                 DISPLAYSURF.blit(walkRight[self.walkCount // 3], (self.x,self.y))
                 self.walkCount +=1
             elif self.isJump:
                 DISPLAYSURF.blit(jump[self.walkCount // 5], (self.x, self.y))
                 self.walkCount += 1
-                print(self.walkCount)
-
         else:
-            if self.right:
-                DISPLAYSURF.blit(walkRight[0], (self.x, self.y))
-            else:
+            if self.left:
                 DISPLAYSURF.blit(walkLeft[0], (self.x, self.y))
+            else:
+                DISPLAYSURF.blit(walkRight[0], (self.x, self.y))
 
 class projectile(object):
-    def __init__(self,x,y,radius,color,facing):
+    def __init__(self,x,y,facing):
         self.x = x
         self.y = y
-        self.radius = radius
-        self.color = color
         self.facing = facing
         self.value = 8 * facing
 
     def draw(self):
-        pygame.draw.circle(DISPLAYSURF, self.color, (self.x,self.y), self.radius)
+        prop = pygame.image.load(paht_img1 + 'prop.png')
+        DISPLAYSURF.blit(prop, (self.x, self.y))
+
+def bull(bullets):
+    for bullet in bullets:
+        if bullet.x < 800 and bullet.x > 0:
+            bullet.x += bullet.value
+        else:
+            bullets.pop(bullets.index(bullet))
 
 def redrawGameWindow():
     ninja.draw()
+
+    for bullet in bullets:
+        bullet.draw()
+    
     pygame.display.update()
 
-def update(key):
+def update(keys, bullets):
+
+    if keys[pygame.K_SPACE]:
+        if ninja.left:
+            facing = -1
+        else:
+            facing = 1
+        
+        if len(bullets) < 10:
+            bullets.append(projectile(
+                round(ninja.x + 50), 
+                round(ninja.y + 50),
+                facing
+            ))
+
     if keys[pygame.K_LEFT] and ninja.x > ninja.value:
         ninja.x -= ninja.value
         ninja.left = True
         ninja.right = False
         ninja.standing = False
-    elif keys[pygame.K_RIGHT] and ninja.x < 500 - ninja.width - ninja.value:
+    elif keys[pygame.K_RIGHT] and ninja.x < 800 - ninja.width - 50  - ninja.value:
         ninja.x += ninja.value
         ninja.right = True
         ninja.left = False
         ninja.standing = False
-    elif keys[pygame.K_SPACE]:
+    elif keys[pygame.K_UP]:
         ninja.standing = False
     else:
         ninja.standing = True
         ninja.walkCount = 0
         
     if not(ninja.isJump):
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_UP]:
             ninja.isJump = True
             ninja.right = False
             ninja.left = False
@@ -148,6 +169,8 @@ def update(key):
             ninja.jumpCount = 10
 
 ninja = Ninja(0, 400, 64, 64)
+bullets = []
+
 run = True
 
 while run:
@@ -158,9 +181,11 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     
+    bull(bullets)
+
     keys = pygame.key.get_pressed()
 
-    update(keys)
+    update(keys, bullets)
     DISPLAYSURF.fill(WHITE)
     redrawGameWindow()
 
