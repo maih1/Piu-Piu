0403b30834f76769d4f454ae13bde6b16c21b106
kaioz11 <hoangmai.tq2000@gmail.com>
2021-01-13 13:06:05 +0700

    fix quit

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+            pygame.quit()
+            quit()
+    
     updateBg()
-
-pygame.quit()
